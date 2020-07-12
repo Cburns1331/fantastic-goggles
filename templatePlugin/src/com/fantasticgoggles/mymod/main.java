75febbf7638ea792d93f3a13c11a2d03cc424258
Taylor Henderson <tph5595@verizon.net>
2020-07-11 22:51:15 -0400

    finist adding example project

--- templatePlugin/src/com/fantasticgoggles/mymod/main.java
+++ templatePlugin/src/com/fantasticgoggles/mymod/main.java
@@ -4,13 +4,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class main extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
-        // TODO Insert logic to be performed when the plugin is enabled
-    }
-    
-    @Override
-    public void onDisable() {
-        // TODO Insert logic to be performed when the plugin is disabled
-    }
+	@Override
+	public void onEnable() {
+		// TODO Insert logic to be performed when the plugin is enabled
+
+		// Commands enabled with following method must have entries in plugin.yml
+		getCommand("example").setExecutor(new command(this));
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO Insert logic to be performed when the plugin is disabled
+	}
 }
