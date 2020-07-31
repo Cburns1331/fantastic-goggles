4c44b75a55e25695eb4fa8c4eaac7a3705676a26
Chris Burns <burns.christopher13@gmail.com>
2020-07-30 22:45:33 -0400

    Update to fall damage/toggleable command
    Messed up plugin trying to implement CircleCI, just started over and figured out a way to negate fall damage without slow falling.
    Also made command have an on/off by re-using command.
    Implemented some basic error checking with command entry.
    Has 1 warning, just need to delete line.
    Couple issues - I want to look into a better solution to 'fog' blindness is meh.
    -Currently when you come out of frogmode you take .5 heart of damage. Need to figure out why

--- foggyFrog/src/com/fantasticgoggles/mymod/FrogMode.java
+++ foggyFrog/src/com/fantasticgoggles/mymod/FrogMode.java
@@ -9,6 +9,7 @@ import org.bukkit.potion.PotionEffectType;
 
 public class FrogMode implements CommandExecutor {
     FoggyFrog plugin;
+    boolean isFrog = false;
 
     public FrogMode(FoggyFrog plugin) {
         this.plugin = plugin;
@@ -17,21 +18,37 @@ public class FrogMode implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         String cmdName = cmd.getName().toLowerCase();
+        PotionEffect frogJump = new PotionEffect(PotionEffectType.JUMP, Integer.MAX_VALUE, 4, false, true, false);
+        PotionEffect frogFog = new PotionEffect(PotionEffectType.BLINDNESS, Integer.MAX_VALUE, 1, false, true, false);
+        PotionEffect frogFall = new PotionEffect(PotionEffectType.SLOW_FALLING, Integer.MAX_VALUE, (int) .5, false, false, false);
 
-        if (cmdName.equals("frogmode")) {
-        	if (sender instanceof Player) {
-        		sender.sendMessage("Successfully used frogmode command!");
-        		PotionEffect frogJump = new PotionEffect(PotionEffectType.JUMP, Integer.MAX_VALUE, 4, false, true, false);
-        		((Player) sender).addPotionEffect(frogJump, true);
-        		return true;
-        	}
-        	else {
+        if (cmdName.equals("frogmode") && isFrog == false && args.length == 0 && sender instanceof Player) {
+        	sender.sendMessage("Successfully used frogmode command!");
+        	isFrog = true;
+        	((Player) sender).addPotionEffect(frogJump, true);
+        	((Player) sender).addPotionEffect(frogFog, true);
+        	//((Player) sender).addPotionEffect(frogFall, true);
+        	((Player) sender).setFallDistance(-1000000);
+        	return true;
+        }
+        else if(cmdName.equals("frogmode") && isFrog == true && args.length == 0 && sender instanceof Player) {
+        	sender.sendMessage("Disabling frogmode...");
+        	isFrog = false;
+        	((Player) sender).removePotionEffect(PotionEffectType.JUMP);
+        	((Player) sender).removePotionEffect(PotionEffectType.BLINDNESS);
+        	//((Player) sender).removePotionEffect(PotionEffectType.SLOW_FALLING);
+        	((Player) sender).setFallDistance((float) 3.7);
+        	return true;
+        }
+        else {
+        	if(!(sender instanceof Player)) {
         		sender.sendMessage("Only players can use this command!");
         		return false;
         	}
-        }
-        else {
-        	return false;
+        	else {
+        		sender.sendMessage("Error with command, please try again /frogmode");
+        		return false;
+        	}
         }
     }
 }
